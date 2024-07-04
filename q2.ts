462d1817c23617e9706ffe444c6a7b86337af21f
Niphitpon Thosap <NiphitponT.63@cmu.ac.th>
2024-07-04 16:31:41 +0700

    ส่งงาน

--- q2.ts
+++ q2.ts
@@ -1,10 +1,20 @@
 // define interface for Student object
-/* Your code here */
 
-// assign interface/type to the function definition properly
-function findTopNames(students) {
-  /* Your code here */
+import { isJSDocReturnTag } from "typescript";
+
+/* Your code here */
+interface Student {
+  name : string;
+  score : number;
 }
+// assign interface/type to the function definition properly
+function findTopNames(students: Student[]){
+  /* Your code here */
+  const Find = students.filter((std) => std.score >8)
+  .map ((std) => std.name);
+  return Find;
+}
+
 
 // assign interface/type to the student1 object properly
 const students1 = [
